b2284ab9698aca30edc8f4f3c00d36f86dd5fba0
Corey Sather <corey042@gmail.com>
2023-01-31 09:51:30 -0600

    Use .doNotTrack when applicable

--- content/Reports/VendorReviewSchedule.php
+++ content/Reports/VendorReviewSchedule.php
@@ -138,7 +138,7 @@ class VendorReviewSchedule extends PageLayoutA
                 LEFT JOIN is4c_op.prodReview AS pr ON pr.upc=p.upc
             WHERE p.inUse = 1
                 AND p.last_sold > DATE_SUB(NOW(), INTERVAL 2 MONTH)
-                AND v.vendorID NOT IN ( 1, 2, 70, 7, 22, 23, 25, 28, 30, 35, 38, 42, 61, 65,127,143,146,147,171,191,196,228,230,232,263,264,358,374,401)
+                AND v.vendorID NOT IN ( 1, 2, 70, 7, 22, 23, 25, 28, 30, 35, 38, 42, 61, 65,127,143,146,147,171,191,196,228,230,232,263,264,358,374,401,414 )
                 AND v.vendorID NOT IN (SELECT vid FROM woodshed_no_replicate.top25)
             GROUP BY sch.vendorID, sch.month
             ORDER BY sch.month ASC, count(p.upc) DESC
@@ -290,7 +290,7 @@ HTML;
                 AND vendorID NOT IN (SELECT vendorID FROM woodshed_no_replicate.FixedVendorReviewSchedule)
                 AND vendorID NOT IN (SELECT vid FROM woodshed_no_replicate.top25)
                 AND vendorID > 0
-                AND vendorID NOT IN (54, 70, 260, 401)
+                AND v.vendorID NOT IN (SELECT upc FROM woodshed_no_replicate.doNotTrack WHERE page = 'VendorReviewSchedule' AND method = 'getMissingVendors')
                 AND m.super_name NOT IN ('PRODUCE', 'BRAND', 'MISC')
                 AND p.numflag & (1<<19) = 0
             GROUP BY v.vendorID
